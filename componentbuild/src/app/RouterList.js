554c4d3668ecec1c3b5281e19db2fc1106985975
Dan <d5361920@gmail.com>
2023-01-19 09:21:20 +0800

    update Routerlist and add url mapping

--- componentbuild/src/app/RouterList.js
+++ componentbuild/src/app/RouterList.js
@@ -1,13 +1,16 @@
 import { Grid } from "@mui/material";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { CheckBox } from "./component/checkBox/CheckBox";
 import { Selector } from "./component/selector/Selector";
 
 export function RouterList(props) {
   const [value, setValue] = useState("");
-  const changeValue = (value) => {
+  const changeValue = value => {
     setValue(value);
   };
-  let element = (
+
+  let SelectElement = (
     <Grid container>
       <Grid item xs={2} marginLeft={10}>
         <Selector
@@ -17,8 +20,29 @@ export function RouterList(props) {
           value={value}
         />
       </Grid>
-      <Grid item xs={2} marginLeft={-10}></Grid>
+      <Grid item xs={2} marginLeft={-10}>
+        <Selector
+          onChange={changeValue}
+          label="testSelect"
+          list={["four", "five", "six"]}
+          value={value}
+        />
+      </Grid>
     </Grid>
   );
-  return element;
+
+  let checkBoxElement = (
+    <Grid container>
+      <Grid item xs={3} marginLeft={10}>
+        <CheckBox label="A" />
+      </Grid>
+    </Grid>
+  );
+
+  return (
+    <Routes>
+      <Route path="/testSelect" element={SelectElement} />
+      <Route path="/testCheckBox" element={checkBoxElement} />
+    </Routes>
+  );
 }
